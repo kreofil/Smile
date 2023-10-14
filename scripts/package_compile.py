dd0660879918a48a04c315068032bd5d4ad01d37
kreofil <kreofil@mail.ru>
2023-10-14 23:29:11 +0300

    Убрал лишние файлы tex

--- scripts/package_compile.py
+++ scripts/package_compile.py
@@ -23,6 +23,9 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
+
+# use: package_compile <path to package>
+
 # import random
 import sys
 # import time
